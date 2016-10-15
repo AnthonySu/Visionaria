b4e48c6fafb7b6dcea90134a37c34969c8f40e91
JoannaN <joanna.ng@berkeley.edu>
2016-10-15 04:36:54 +0000

    added iter1-1 features and step_defs

---
+++ features/step_definitions/post_steps.rb
@@ -0,0 +1,16 @@
+Given /the following posts exist/ do |table|
+  # table is a Cucumber::MultilineArgument::DataTable
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When /press "([^"]*)" on the post with id "([^"]*)"/ do |action, post_id|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then /should see "([^"]*)" points? for "([^"]*)"/ do |num_points, goal|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When /follow the tag "([^"]*)"/ do |tag_name|
+  pending # Write code here that turns the phrase above into concrete actions
+end
