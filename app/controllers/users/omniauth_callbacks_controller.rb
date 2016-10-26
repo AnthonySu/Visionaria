b1f460862ea867d1c7084547e7555cc22a848133
JoannaN <joanna.ng@berkeley.edu>
2016-10-26 13:13:37 +0000

    added redirect in omniauthcallback_controller

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,20 +1,18 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     @user = User.from_omniauth(request.env["omniauth.auth"])
-    puts @user.errors.to_a
     if @user.persisted?
-      puts "USER PERSISTED"
-      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+      sign_in(@user)
+      redirect_to post_index_path, notice: 'Signed in successfully.'
+      #sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
     else
-      puts "Actually, USER NOT PERSISTED"
       session["devise.facebook_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
     end
   end
 
   def failure
-    puts "Rootpath: IS HERE: "+root_path
     redirect_to root_path
   end
 end
