29ab31ac8e01ec8e1f54e7a767346f630b3a9638
JoannaN <joanna.ng@berkeley.edu>
2016-11-13 10:40:04 +0000

    cleaned up code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,3 @@
 class UsersController < ApplicationController
     before_filter :authenticate_user!
-    
-    #def show
-    #    @posts = current_user.posts.where('public = ? OR user_id = ?', true, current_user.id).order('created_at DESC')
-    #    @tagged_posts = current_user.tagged_posts.where('public = ? OR user_id = ?', true, current_user.id).order('created_at DESC')
-    #end
 end
